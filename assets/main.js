768735bda8bb3e33bc8bd8612b9d9dceb479527f
OGUNLEYE OLANREWAJU EZEKIEL <103619061+larryWEB@users.noreply.github.com>
2024-11-05 21:09:11 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("NOV 3, 2024 00:00:00").getTime();
+let countDownDate = new Date("NOV 10, 2024 00:00:00").getTime();
 let a = setInterval(function(){
     let now =new Date().getTime();
     let distance = countDownDate -now;
